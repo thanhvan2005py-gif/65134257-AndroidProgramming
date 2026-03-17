ddfc9262463d4571a5682aaee3ce41bf91e6323f
thanhvan2005py-gif <thanhvan2005py@gmail.com>
2026-03-17 19:53:14 +0700

    Hoàn Thành

--- BaiTH5_XuLySuKien1/app/src/main/java/van/baith5_xulysukien1/MainActivity.java
+++ BaiTH5_XuLySuKien1/app/src/main/java/van/baith5_xulysukien1/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         nutTru.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //gọi hàm xử lý
+                //gọi hàm xử lý trừ
                 XULY_TRU();
             }
         });
