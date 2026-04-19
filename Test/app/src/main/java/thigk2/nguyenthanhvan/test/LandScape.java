73003006d79816997c896eb20c6418fa5103dc47
thanhvan2005py-gif <thanhvan2005py@gmail.com>
2026-04-19 12:46:07 +0700

    Hoàn Thành

---
+++ Test/app/src/main/java/thigk2/nguyenthanhvan/test/LandScape.java
@@ -0,0 +1,36 @@
+package thigk2.nguyenthanhvan.test;
+
+public class LandScape {
+    String landImageFileName;
+    String landCation;
+    String moTa;
+
+    public LandScape(String landImageFileName,String landCation,String moTa ) {
+        this.landCation = landCation;
+        this.landImageFileName = landImageFileName;
+        this.moTa = moTa;
+    }
+
+    public String getLandCation() {
+        return landCation;
+    }
+
+    public void setLandCation(String landCation) {
+        this.landCation = landCation;
+    }
+
+    public String getLandImageFileName() {
+        return landImageFileName;
+    }
+
+    public void setLandImageFileName(String landImageFileName) {
+        this.landImageFileName = landImageFileName;
+    }
+    public String getMoTa() {
+        return moTa;
+    }
+
+    public void setMoTa(String moTa) {
+        this.moTa = moTa;
+    }
+}
