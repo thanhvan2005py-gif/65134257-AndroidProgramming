8508c4c1740b9b0456f64c799c94da8ec8736fe3
thanhvan2005py-gif <thanhvan2005py@gmail.com>
2026-04-20 08:03:19 +0700

    Lần Thứ 2

--- GiuaKi2/app/src/main/java/thigk2/nguyenthanhvan/giuaki2/Cau2Fragment.java
+++ GiuaKi2/app/src/main/java/thigk2/nguyenthanhvan/giuaki2/Cau2Fragment.java
@@ -1,5 +1,6 @@
 package thigk2.nguyenthanhvan.giuaki2;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -33,7 +38,38 @@ public class Cau2Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cau2, container, false);
+        View view = inflater.inflate(R.layout.fragment_cau2, container, false);
+        dsTinh= new ArrayList<String>();// Tạo thể hiện cụ thể , xin mới
+        //thêm dữ liệu
+        dsTinh.add("Nguyễn Thành Văn");
+        dsTinh.add("Hà Nội");
+        dsTinh.add("Thành Phố HCM");
+        dsTinh.add("Thành Phố HCM");
+        dsTinh.add("Thành Phố HCM");
+        dsTinh.add("Thành Phố HCM");
+        dsTinh.add("Thành Phố HCM");
+        dsTinh.add("Thành Phố HCM");
+        dsTinh.add("Thành Phố HCM");
+        dsTinh.add("Thành Phố HCM");
+
+
+        ArrayAdapter<String> adapterMonAn;
+        adapterMonAn = new ArrayAdapter<>(getContext(),
+                R.layout.item_center,
+                R.id.tvItem,
+                dsTinh
+        );
+        ListView lvTenMonAn = view.findViewById(R.id.lvDanhSachTinh);
+        lvTenMonAn.setAdapter(adapterMonAn);
+        lvTenMonAn.setOnItemClickListener(BoLangNgheVaXL);
+        return view;
     }
+    AdapterView.OnItemClickListener BoLangNgheVaXL = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+            String strTenTinh = dsTinh.get(i);
+           Toast.makeText(getContext(), strTenTinh, Toast.LENGTH_SHORT).show();
+
+        }
+    };
 }
--- GiuaKi2/app/src/main/java/thigk2/nguyenthanhvan/giuaki2/Cau3Fragment.java
+++ GiuaKi2/app/src/main/java/thigk2/nguyenthanhvan/giuaki2/Cau3Fragment.java
@@ -3,14 +3,20 @@ package thigk2.nguyenthanhvan.giuaki2;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class Cau3Fragment extends Fragment {
 
-
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> list;
+    RecyclerView recyclerViewLandScape;
     public Cau3Fragment() {
         // Required empty public constructor
     }
@@ -25,12 +31,28 @@ public class Cau3Fragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        list = new ArrayList<LandScape>();
+        list.add (new LandScape("flaghanoi","Tỉnh Hà Nội","Địa điểm"));
+        list.add(new LandScape("eiffel","Tỉnh Nha Trang","Địa điểm"));
+        list.add(new LandScape("buckingham","Phú Yên","Địa điểm"));
+        list.add(new LandScape("nhatrang","Nha Trang","Địa điểm"));
+        list.add(new LandScape("haugiang","Hậu Giang","Địa điểm"));
+        list.add(new LandScape("nhatrang","So Trăng","Địa điểm"));
+        list.add(new LandScape("nhatrang","Nha Trang","Địa điểm"));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cau3, container, false);
+        View view = inflater.inflate(R.layout.fragment_cau3, container, false);
+        recyclerViewLandScape = view.findViewById(R.id.recyclerViewCau3);
+        //
+        RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(getContext());;
+        recyclerViewLandScape.setLayoutManager(layoutLinear);
+        //
+        LandScapeAdapter adapter = new LandScapeAdapter(view.getContext(),list);
+        //gan vao recycler view
+        recyclerViewLandScape.setAdapter(adapter);
+        return view;
     }
 }
