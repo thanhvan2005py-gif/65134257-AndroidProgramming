cca91d0712b5aecafa402c01fb36675a979ed4f0
thanhvan2005py-gif <thanhvan2005py@gmail.com>
2026-05-06 08:56:24 +0700

    Hoàn Thành

--- ViecCanLam/app/src/main/java/vn/edu/tinhoc123/vieccanlam/MainActivity.java
+++ ViecCanLam/app/src/main/java/vn/edu/tinhoc123/vieccanlam/MainActivity.java
@@ -1,7 +1,9 @@
 package vn.edu.tinhoc123.vieccanlam;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -9,6 +11,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,6 +26,8 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     List<TASKS> lstVCL;
+    TaskRVadapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,24 +39,35 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         // tạo kết nối trên cở sowrduwx liệu Firebase
+        lstVCL = new ArrayList<TASKS>();
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = database.getReference("tasks");
-        // lắng nghe và xử lý
-        lstVCL = new ArrayList<TASKS>();
-        databaseReference.addValueEventListener(new ValueEventListener() {
+
+        databaseReference.addValueEventListener(ngheFB);
+        // tim dieu khien
+        RecyclerView recyclerView = findViewById(R.id.rcvVCL);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+        adapter = new TaskRVadapter(lstVCL);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+           ValueEventListener ngheFB = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                lstVCL.clear();
                 // lay du lieu
                 for(DataSnapshot obj : snapshot.getChildren()){
                     TASKS task = obj.getValue(TASKS.class);
                     lstVCL.add(task);
-                    Log.w("VCL app", "Tên vệc cần làm: "+task.getName());
+                   // Log.w("VCL app", "Tên vệc cần làm: "+task.getName());
                 }
-
+                adapter.notifyDataSetChanged();
             }
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
-        });
+        };
     }
-}
